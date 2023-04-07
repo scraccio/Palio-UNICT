d27720a7a2d9b5d6192d423ceef769093dd0c852
scraccio <simone.costanzo1337@gmail.com>
2023-04-07 10:40:08 +0200

    chore(gh-pages): delete useless gh-pages files

--- src/app/nav-bar-entry/nav-bar-entry.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component, Input} from '@angular/core';
-
-@Component({
-  selector: 'app-nav-bar-entry',
-  templateUrl: './nav-bar-entry.component.html',
-  styleUrls: ['./nav-bar-entry.component.css']
-})
-export class NavBarEntryComponent {
-}
--- src/app/page-title/page-title.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-page-title',
-  templateUrl: './page-title.component.html',
-  styleUrls: ['./page-title.component.css']
-})
-export class PageTitleComponent {
-}
